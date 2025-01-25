eadf689fdafd5b61781b56dae604a0f459c6e458
Kovalski Pingvin <kowalskipingvin@outlook.com>
2025-01-25 12:24:55 +0100

    Create Mekgafijum239.sh

---
+++ Mekgafijum239.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+mekgafijumvers="239"
+
+echo "Hello, please introduce yourself."
+
+echo -n "Your name: "
+read -r name
+
+echo "Are you a scientist?"
+
+echo -n "y/n: "
+read -r human
+
+echo "Do you want to proceed to Mekgafijum-$megkafijumvers?"
+
+echo -n "Your answer: "
+read -r lang
+
+echo
+echo "Answers:"
+echo "1. $name"
+echo "2. $human"
+echo "3. $lang"
